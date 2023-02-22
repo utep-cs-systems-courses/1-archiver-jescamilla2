fdb8bbbda46610fef8f35403e85918b0203c092a
jescamilla2 <jescamilla2@miners.utep.edu>
2023-02-21 23:50:55 -0500

    completed

--- mytar.sh
+++ mytar.sh
@@ -11,14 +11,13 @@ def framed_write(data):
     ''' 
     fdata = bytearray()                              # byte-array for framed data
     size = len(data)                                 # len of data as byte-array
-    
     fdata += bytearray(size.to_bytes(8, 'big'))      # uses big-endian byte order
     fdata += bytearray(data)                         # converts the message to bytes
 
     return fdata
 
 
-def framed_read(fdata, fnum=1):
+def framed_read(fdata):
     '''
     function: framed_read()
     input: Takes a framed msg, fdata, as a byte-array and extracts data where the
@@ -26,19 +25,26 @@ def framed_read(fdata, fnum=1):
         fdata[0:8] = data length in bytes
         fdata[8:] = data contents
         fnum is number of framed pieces to read
-    returns an array: [data1, data2, ... , ]
+    returns an array: [data1, data2, ... , ] and sliced framed-data
     '''    
     if len(fdata) == 0:
-        return byte_array()
+        return [], byte_array()
     
-    data = []
+    data = ['', bytearray()]
     
-    for i in range(fnum):                             # read fnum frames
-        size = int.from_bytes(fdata[0:8], 'big')      # get fdata size as bytes   
-        msg[i] = data[8:8+size].decode()              # get fdata contents as bytes
-        fdata = fdata[8+size:]                        # update fdata with the next slice
+    # read the filename
+    size = int.from_bytes(fdata[0:8], 'big')      # get fdata size as bytes
+    contents = fdata[8:8+size].decode()
+    data[0] = contents
+    fdata = fdata[8+size:]                        # update fdata with the next slice
 
-    return data
+    # read the file contents
+    size = int.from_bytes(fdata[0:8], 'big')
+    contents = fdata[8:8+size]
+    data[1] += contents
+    fdata = fdata[8+size:]
+    
+    return data, fdata
 
 
 def read_from_fd(fd, n=100):
@@ -77,31 +83,24 @@ def c(files, ofd=1):
         try:
             fdata = bytearray()
             ifd = os.open(file, os.O_RDONLY)                  # open file (read-only)
-            print(f'opening file {file} with fd={ifd}')
-
-            print(f'file length is {len(file.encode())}')
-            print(f'file name is {file}')
-            
+           
             fmetadata = framed_write(file.encode())           # frame filename
             fdata += fmetadata                                # add framed metadata (filename) to byte array
-            print(f'fdata is now {fdata}')
             
             contents = read_from_fd(ifd, 10)                  # read 10 bytes (at a time) from open fd
             fcontents = framed_write(contents)                # frame contents from input fd
             fdata += fcontents                                # add framed contents to byte array
-            print(f'adding {fcontents.decode()} to byte array')
 
             os.write(ofd, fdata)                              # send data to output fd
             os.close(ifd)                                     # done with input file descriptor
-            print(f'closing file {file} with fd={ifd}')
+
         except:
             print('File does not exist')
 
     os.close(ofd)                                             # close output stream
 
-
     
-def x(ifd=0, ofd=1):
+def x(file):
     '''
     input: ifd is input file descriptor (default 0)
         data from ifd should be framed data
@@ -109,33 +108,18 @@ def x(ifd=0, ofd=1):
     '''
     try:
 
+        ifd = os.open(file, os.O_RDONLY)
+        fdata = read_from_fd(ifd)
 
+        while len(fdata) > 0:
 
-        arch_file = open(file, 'rb')                          # 'rb' = 'read binary'
-        data = arch_file.read()
+            data, fdata = framed_read(fdata)               # read 2 pieces of framed data (filename, contents)
 
-        while len(data) > 0:                                  # continue through entire file
-            fname_len = int.from_bytes(data[:8], 'big')       # len of filename
-            fname = data[8:8+fname_len]                       # actual name of file (in bytes)
-            data = data[8+fname_len:]                         # remove fname_len and fname
+            ofd = os.open(data[0], os.O_WRONLY | os.O_CREAT)  # create file to write to if necessary
+            os.write(ofd, data[1])
+            os.close(ofd)
 
-            fcontents_len = int.from_bytes(data[:8], 'big')   # len of fcontents
-            fcontents = data[8:8+fcontents_len]               # actual file contents (in bytes)
-            data = data[8+fcontents_len:]                     # remove fcontents_len and fcontents
-
-            new_file = open(fname.decode(), 'wb')             # create new file
-            new_file.write(fcontents)                         # write the extracted file contents
-            new_file.close()
-
-            """
-            alternatively, you can just open the file 
-            with 'w' and instead decode fcontents first.
-
-            new_file = open(fname.decode(), 'w')
-            new_file.write(fcontents.decode())
-            """
-
-        arch_file.close()                                     # done with the archived file
+        os.close(ifd)                                         # done with the archived file
             
     except FileNotFoundError:
         os.write(1, f'{file}: File not found. Exiting...'.encode())
@@ -153,10 +137,10 @@ elif sys.argv[1] == 'c':
     c(files, fd)
     print('files have been archived to {}'.format(sys.argv[-1]))
 
-elif sys.argv[1] == 'unarch':
+elif sys.argv[1] == 'x':
     print('extracting files...')
     file = sys.argv[-1]
-    unarch(file)
+    x(file)
     print('files have been extracted')
 
 else:
